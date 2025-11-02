37bea33c111834d5df1b0b7604e2054d34f9bcd3
dhiraj312003 <dhirajmahajan.eng@gmail.com>
2025-11-02 17:50:15 +0530

    Delete Course.java

--- Course.java
+++
@@ -1,8 +0,0 @@
-public class Course {
-    public static void main (String[] args) {
-        int age =1;
-        for ( age = 1, age < 11, age++){
-             System.out.println("Age :" + age)
-        }
-    }
-}
